dfe3952f66b41d087fe74ab87ac6f083a1773283
Rafael Jesus <Rafael.Neves.Jesus@gmail.com>
2020-10-05 01:43:08 +0100

    SeedDb

--- ShopCET47.web/Data/Entities/Product.cs
+++ ShopCET47.web/Data/Entities/Product.cs
@@ -11,7 +11,7 @@ namespace ShopCET47.web.Data.Entities
 
         public int id { get; set; }
 
-
+        [Required]
         public string Name { get; set; }
 
 
@@ -24,11 +24,11 @@ namespace ShopCET47.web.Data.Entities
 
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Is Available")]
--- ShopCET47.web/Migrations/20201004235401_modifyproducts.Designer.cs
+++ ShopCET47.web/Migrations/20201004235401_modifyproducts.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShopCET47.web.Data;
 
 namespace ShopCET47.web.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201004235401_modifyproducts")]
+    partial class modifyproducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
